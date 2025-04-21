be29d622690672b2878fdd73537f650533fff9bd
stenowarriorstranscriptionsoftware <stenowarriortranscriptionsoftw@gmail.com>
2025-04-21 09:01:20 +0530

    Add files via upload

---
+++ lookup.js
@@ -0,0 +1,165 @@
+// Video URL lookup
+const urlLookup = {
+  1: {
+    80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+    100: "https://www.youtube.com/embed/yzab5678"
+  },
+  2: {
+    80: "https://www.youtube.com/embed/Q_5_aLlUQ9k?si=gaKn2YcDCYPZKcCA",
+    100: "https://www.youtube.com/embed/yzab5678"
+  },
+  3: {
+    80: "https://www.youtube.com/embed/f-PMpwoOQRE?si=u4KMBZT_pEfkWDcy",
+    100: "https://www.youtube.com/embed/yzab5678"
+  },
+  4: {
+    80: "https://www.youtube.com/embed/98BboChV5Lk?si=25g0aAdV-C0SZQsq",
+    100: "https://www.youtube.com/embed/yzab5678"
+  },
+  5: {
+    80: "https://www.youtube.com/embed/nHpWLJbXrsw?si=e--vcAvUzyVzvp6O",
+    100: "https://www.youtube.com/embed/yzab5678"
+  },
+ 6: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+7: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+8: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+9: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+10: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+11: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+12: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+13: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+14: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+15: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+16: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+17: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+18: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+19: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+20: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+21: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+22: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+23: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+24: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+25: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+26: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+27: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+28: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+29: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+30: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+31: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+32: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+33: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+34: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+35: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+36: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+37: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+38: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+39: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+40: {
+  80: "https://www.youtube.com/embed/cTDROhAw5Fg?si=LSJ5amgvUa2W4m_1",
+  100: "https://www.youtube.com/embed/yzab5678"
+},
+// Continues from 41 to 100 in same format...
+
+};
